529835abad8d082db6ee2417a56573def61afcaf
wisemuji <wisemuji@gmail.com>
2019-05-18 21:19:45 +0900

    서버 시작

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import config from './config';
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    limit: '1gb',
+    extended: false
+}));
+
+//module setting
+// import { Users, Groups, Boards, Scans } from './mongo';
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+
+//서버 실행
+const PORT = config.PORT || 5000;
+app.listen(PORT, function() {
+    console.log('server running in ' + PORT);
+});
+
+// require('./routes/index')(app);
